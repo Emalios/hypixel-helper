5d647a99570abe07fa9712e90693bddc485af427
Hugo Bergerat <hugo.bergerat2002@gmail.com>
2022-10-05 23:15:28 +0200

    get data from hypixel api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { BazaarComponent } from './bazaar/bazaar.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'bazaar', component: BazaarComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ]
 
@@ -16,11 +20,15 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    BazaarComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
